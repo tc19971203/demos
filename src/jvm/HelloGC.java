b6e711798b09a30395dbc5eed484ca5adaf2b980
tangchen <tc15542726981@163.com>
2021-08-16 17:32:25 +0800

    gc

---
+++ src/jvm/HelloGC.java
@@ -0,0 +1,8 @@
+package jvm;
+
+public class HelloGC {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("******hello gc");
+       // byte[] bytes = new byte[50*1024*1024];
+    }
+}
